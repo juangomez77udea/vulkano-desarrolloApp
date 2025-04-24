74c0608b03e204d1145414370681f7d69de6d564
Juan Carlos Gómez <juan.gomez77@udea.edu.co>
2025-04-24 11:56:04 -0500

    Primer commit

---
+++ src/main/java/com/vulkano/desarolloApp/DesarolloAppApplication.java
@@ -0,0 +1,13 @@
+package com.vulkano.desarolloApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarolloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarolloAppApplication.class, args);
+	}
+
+}
